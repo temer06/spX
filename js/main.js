2c7c575fe4a9a84003038ec424d29b588c2e1977
temer06 <61481236+temer06@users.noreply.github.com>
2020-08-26 07:33:22 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var instance = new vidbg('.video', {
+  mp4: 'video/world.mp4', // URL or relative path to MP4 video
+  webm: 'video/world.webm', // URL or relative path to webm video
+  poster: 'poster.jpg', // URL or relative path to fallback image
+  overlay: false, // Boolean to display the overlay or not
+  overlayColor: '#000', // The overlay color as a HEX
+  overlayAlpha: 0.3 // The overlay alpha. Think of this as the last integer in RGBA()
+}, {
+  // Attributes
+})
+var rellax = new Rellax('.rocket');
+// if(document.rocket.clientWidth<883){
+  AOS.init();
+
+  $(document).ready(function(){
+    $('.header__burger').click(function(event){
+      $('.header__burger,.header__menu').toggleClass('active');
+      $('body').toggleClass('lock');
+    });
+  });
